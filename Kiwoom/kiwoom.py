49f01ed2704fb0f5d5e6a3924093eee1116457c6
도츠 <starfish0908@naver.com>
2021-06-09 00:02:59 +0900

    예수금 조회 구현

--- Kiwoom/kiwoom.py
+++ Kiwoom/kiwoom.py
@@ -1,10 +1,15 @@
 from PyQt5.QAxContainer import *
 from PyQt5.QtCore import *
+from config.errorCode import *
+import os
 
 class Kiwoom(QAxWidget):
     def __init__(self):
         super().__init__()
         print("[+] Kiwoom init called")
+
+        #init values
+        self.account_num = ''
 
         ##### eventloop 모음
         self.login_event_loop = None
@@ -13,17 +18,22 @@ class Kiwoom(QAxWidget):
         self.event_slots()
         self.signal_login_commConnect()
 
+        self.get_acoount()
+        self.detail_account()
+
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
     def event_slots(self):
         self.OnEventConnect.connect(self.login_slot)
+        self.OnReceiveTrData.connect(self.tr_slot)
 
     def login_slot(self, errorCode):
         if errorCode == 0:
             print("connected")
         else:
             print("disconnected")
+        print(errors(errorCode))
 
         self.login_event_loop.exit()
 
@@ -32,3 +42,43 @@ class Kiwoom(QAxWidget):
 
         self.login_event_loop = QEventLoop()
         self.login_event_loop.exec_()
+
+    def get_acoount(self):
+        account_list = self.dynamicCall("GetLoginInfo(String)","ACCNO")
+        self.account_num = account_list.split(';')[0]
+        user_name = self.dynamicCall("GetLoginInfo(String)","USER_NAME")
+        server_gubun = self.dynamicCall("GetLoginInfo(String)","GetServerGubun")
+
+        print('%s is login , 계좌번호 %s \nserver %s connected' % (user_name,self.account_num,server_gubun))
+
+    def detail_account(self):
+        if not os.path.exists('credentials.txt'):
+            print('credentials is not exists')
+        else:
+            print('예수금 확인')
+            with open('credentials.txt','r') as f:
+                self.dynamicCall("SetInputValue(String, String)","계좌번호", self.account_num)
+                self.dynamicCall("SetInputValue(String, String)","비밀번호", f.readline())
+                self.dynamicCall("SetInputValue(String, String)","비밀번호입력매체구분", f.readline())
+                self.dynamicCall("SetInputValue(String, String)","조회구분", "2")
+                self.dynamicCall("CommRqData(String, String, int, String)","예수금상세현황","opw00001","0","1000")
+
+
+    def tr_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
+        '''
+        tr요청 받는 slot
+        :param sScrNo: 스크린번호
+        :param sRQName: 요청명
+        :param sTrCode: tr코드
+        :param sRecordName: 사용 X 
+        :param sPrevNext: 다음 페이지 여부
+        :return: 
+        '''
+
+        if sRQName == "예수금상세현황":
+            deposit = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "예수금")
+            print("예수금 %s" % deposit)
+
+
+
+
